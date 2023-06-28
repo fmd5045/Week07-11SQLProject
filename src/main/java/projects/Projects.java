a1f950541e28b6092f51c283f377a7ab66fde6c4
Freddy Duluc <freddy@Freddys-MacBook-Pro.local>
2023-06-27 21:54:38 -0600

    week 2 first commit

---
+++ src/main/java/projects/Projects.java
@@ -0,0 +1,13 @@
+package projects;
+
+import java.sql.Connection;
+
+import projects.dao.Dbconnection;
+
+public class Projects {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/main/resources/projects-schema.sql
@@ -0,0 +1,56 @@
+DROP TABLE IF EXISTS project_category;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS step;
+DROP TABLE IF EXISTS material;
+DROP TABLE IF EXISTS project;
+
+CREATE TABLE project (
+	project_id INT AUTO_INCREMENT NOT NULL,
+	project_name varchar(128) NOT NULL,
+	estimated_hours	decimal(7,2) NOT NULL,
+	actual_hours decimal(7,2) NOT NULL,
+	difficulty INT NOT NULL,
+	notes text,
+	PRIMARY KEY (project_id)
+	);
+
+CREATE TABLE material (
+	material_id int AUTO_INCREMENT NOT NULL,
+	project_id int NOT NULL,
+	material_name varchar(128) NOT NULL,
+	num_required int NOT NULL,
+	cost decimal(7,2) NOT NULL,
+	PRIMARY KEY (material_id),
+	FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
+	);
+
+
+CREATE TABLE step (
+	step_id	int AUTO_INCREMENT NOT NULL,
+	project_id int NOT NULL,
+	step_text text,
+	step_order int NOT NULL,
+	PRIMARY KEY (step_id),
+	FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
+	);
+
+CREATE TABLE category (
+	category_id	int AUTO_INCREMENT NOT NULL,
+	category_name varchar(128),
+	PRIMARY KEY (category_id)
+	);
+
+CREATE TABLE project_category (
+	project_id	int,
+	category_id	int,
+	FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE,
+	FOREIGN KEY (category_id) REFERENCES category (category_id) ON DELETE CASCADE,
+	UNIQUE KEY (project_id, category_id)
+	);
+
+
+
+
+
+
+
